04bf84775350afd97d30f1eb530bedef82c7b5ee
Johan Djärv Karltorp <johan.djarv.karltorp@gmail.com>
2020-06-27 14:42:31 +0200

    created a single layer dropdown menu

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ReactComponent as BellIcon } from './icons/bell.svg';
 import { ReactComponent as MessengerIcon } from './icons/messenger.svg';
 import { ReactComponent as CaretIcon } from './icons/caret.svg';
@@ -15,8 +15,35 @@ function App() {
           <NavItem icon={<PlusIcon />} />
           <NavItem icon={<BellIcon />} />
           <NavItem icon={<MessengerIcon />} />
+
+          <NavItem icon={<CaretIcon />}>
+              <DropdownMenu/>
+
+          </NavItem>
       </Navbar>
   );
+}
+
+function DropdownMenu() {
+
+    function DropdownItem(props) {
+        return (
+          <a href="#" className="menu-item">
+              <span className="icon-button">{props.leftIcon}</span>
+
+              {props.children}
+
+              <span className="icon-right">{props.rightIcon}</span>
+          </a>
+        );
+    }
+
+    return (
+      <div className="dropdown">
+          <DropdownItem>My Profile</DropdownItem>
+          <DropdownItem leftIcon={<CogIcon />} rightIcon={<ChevronIcon />} >Settings</DropdownItem>
+      </div>
+    );
 }
 
 function Navbar(props) {
@@ -28,11 +55,14 @@ function Navbar(props) {
 }
 
 function NavItem(props) {
+    const [open, setOpen] = useState(false);
+
   return (
     <li className="nav-item">
-      <a href="#" className="icon-button">
+      <a href="#" className="icon-button" onClick={() => setOpen(!open)}>
           {props.icon}
       </a>
+        {open && props.children}
     </li>
   );
 
