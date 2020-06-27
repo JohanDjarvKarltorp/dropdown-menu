17b7c9bef8eecadcabbd8227cc6f5109ef038c0d
Johan Djärv Karltorp <johan.djarv.karltorp@gmail.com>
2020-06-27 13:31:32 +0200

    created a empty navbar

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
 function App() {
-  return ();
+  return (
+      <Navbar></Navbar>
+  );
+}
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <ul className="navbar-nav"></ul>
+    </nav>
+  );
 }
 
 export default App;
